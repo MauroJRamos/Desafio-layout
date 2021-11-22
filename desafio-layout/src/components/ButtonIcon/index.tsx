cc6f55781787280b630aa805e5ad3c72d2cc7e11
MauroJRamos <mauroramosti@gmail.com>
2021-11-22 15:45:25 -0300

    Fix: home

---
+++ desafio-layout/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+
+const ButtonIcon = () => {
+  return (
+      <button className="button"> 
+        <h6>VER CATÁLAGO</h6>
+      </button>
+  );
+};
+
+export default ButtonIcon;
